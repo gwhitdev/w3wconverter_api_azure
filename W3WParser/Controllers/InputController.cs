91bf9635eb774ae90b6a1bc401e26372fcd9d8cb
Gareth Whitley <me@garethwhitley.online>
2022-03-08 21:52:25 +0000

    Init

---
+++ W3WParser/Controllers/InputController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Core.Models.Objects;
+using Services.Services;
+
+namespace W3WParser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InputController : ControllerBase
+    {
+        private ILogger<InputController> _logger;
+
+        public InputController(ILogger<InputController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        [Route("/api/input")]
+        public IActionResult PostInput([FromBody] string body)
+        {
+            _logger.LogInformation("Trying to parse data...");
+            
+            CSVReader reader = new();
+            try
+            {
+                var output = reader.ReadCsvFile(body);
+                return Ok(new[]
+                {
+                    output
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                var output = "No values provided";
+                return BadRequest(new[] {
+                    output
+                });
+            }
+
+            
+        }
+    }
+
+    public class InputTestResponse
+    {
+        public string Text { get; set; }
+    }
+
+    
+
+}
---
+++ W3WParser/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace W3WParser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/api/test")]
+        public IEnumerable<TestResponse> GetTest()
+        {
+            _logger.LogInformation("Received test GET request");
+
+            return new[]
+            {
+                    new TestResponse { Text = "Test response"}
+                };
+        }
+    }
+
+    public class TestResponse
+    {
+        public string Text { get; set; }
+    }
+}
