7bc932eaf382f5191bc7b9070b02eb2c6d914694
Gareth Whitley <me@garethwhitley.online>
2022-03-10 15:47:07 +0000

    Updated release options for documentation XML file and removed some unnecessary usings.

--- W3WParser/Startup.cs
+++ W3WParser/Startup.cs
@@ -1,27 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System.Reflection;
 using System.IO;
 using Microsoft.OpenApi.Models;
 using System.Net.Http.Headers;
-using W3WParser.Models;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using static Microsoft.AspNetCore.Mvc.Versioning.ApiVersionMapping;
-
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Microsoft.Extensions.Options;
 
@@ -102,8 +91,6 @@ namespace W3WParser
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
             app.UseSwaggerUI(options =>
             {
-                //c.SwaggerEndpoint("/swagger/v1/swagger.json", $"v1");
-                //c.SwaggerEndpoint("/swagger/v2/swagger.json", $"v2");
                 options.RoutePrefix = string.Empty;
 
                 foreach (var description in provider.ApiVersionDescriptions)
